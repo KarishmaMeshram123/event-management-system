6fa36ed9ac92500bb073f4018c189fe31758d106
pallavi_wandhare <pallaviwandhare@2003gmail.com>
2026-02-10 11:19:11 +0530

    Add frontend work for Auth system

---
+++ Frontend_Event_mangement_system/frontend_event_management_system/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import BookEvent from "./pages/BookEvent";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/book/:id" element={<BookEvent />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
