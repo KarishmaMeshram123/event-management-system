a60fbfc4a908b008ff5bfb5e7bbff2780ea31632
Sakshi Dhamne <sakshid.cse22@sbjit.edu.in>
2026-02-11 14:54:55 +0530

    Integrate navbar, routing and overall UI and Screenshot of project

--- Frontend_Event_mangement_system/frontend_event_management_system/src/App.jsx
+++ Frontend_Event_mangement_system/frontend_event_management_system/src/App.jsx
@@ -1,17 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import BookEvent from "./pages/BookEvent";
+import About from "./components/About";
+import PrivateRoute from "./components/PrivateRoute";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
-    <BrowserRouter>
+  <BrowserRouter>
+      <Navbar />
+     
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />   {/* 👈 ABOUT ADDED */}
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/book/:id" element={<BookEvent />} />
+         <Route path="/contact" element={<Contact />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
